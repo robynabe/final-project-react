2fe545fbf7d6a32eb630041914c1f7cfd1ed9801
robynabe <64428921+robynabe@users.noreply.github.com>
2020-06-26 14:52:38 -0700

    changed github link

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer>
       Designed and
-      <a href="https://github.com/robynabe/Final-Project-SheCodes"> coded </a>
+      <a href="https://github.com/robynabe/final-project-react"> coded </a>
       by Robyn Abe
     </footer>
   );
