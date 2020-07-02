ac2166eb49aed73ca2607356ba2d94792ad58496
robynabe <64428921+robynabe@users.noreply.github.com>
2020-07-02 11:37:39 -0700

    added weather details from API

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Weather from "./Weather";
-import Form from "./Form";
-import Conversion from "./Conversion";
-import Date from "./Date";
-import Details from "./Details";
-import Footer from "./Footer";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./index.css";
 
 function App() {
-  return (
-    <div className="body">
-      <section className="section">
-        <div className="container">
-          <div className="row">
-            <Weather />
-            <div className="col-sm">
-              <Form />
-              <p className="date-time">
-                <Date />
-              </p>
-              <Details />
-            </div>
-          </div>
-          <Conversion />
-        </div>
-      </section>
-      <Footer />
-    </div>
-  );
+  return <Weather />;
 }
 
 const rootElement = document.getElementById("root");
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
+import Date from "./Date";
+import Footer from "./Footer";
 import axios from "axios";
+import Form from "./Form";
+import Conversion from "./Conversion";
 import { data } from "jquery";
 
 export default function Weather() {
@@ -7,26 +11,73 @@ export default function Weather() {
   const [weatherData, setWeatherData] = useState({});
 
   function handleSubmit(response) {
-    console.log(response.data.main.temp);
+    console.log(response.data);
     setWeatherData({
       temp: Math.round(response.data.main.temp),
       city: response.data.name,
+      description: response.data.weather.description,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      feels: response.data.main.feels_like,
     });
     setReady(true);
   }
   if (ready) {
     return (
-      <div>
-        <div class="col-sm">
-          <h1>{weatherData.city}</h1>
-          <p>Cloudy</p>
-          <img
-            src="http://openweathermap.org/img/wn/10d@2x.png"
-            alt="weather icon"
-          />
-          <h2>{weatherData.temp}</h2>
-          <span class="degrees">°C</span>
-        </div>
+      <div className="body">
+        <section className="section">
+          <div className="container">
+            <div className="row">
+              <div>
+                <div class="col-sm">
+                  <h1>{weatherData.city}</h1>
+                  <p>Cloudy</p>
+                  <img
+                    src="http://openweathermap.org/img/wn/10d@2x.png"
+                    alt="weather icon"
+                  />
+                  <h2>{weatherData.temp}</h2>
+                  <span class="degrees">°C</span>
+                </div>
+              </div>
+              <div className="col-sm">
+                <Form />
+                <p className="date-time">
+                  <Date />
+                </p>
+                <ul>
+                  <li>
+                    <img
+                      src="http://openweathermap.org/img/wn/50d@2x.png"
+                      alt="wind-icon"
+                      class="detail-icons"
+                    />
+                    {weatherData.wind} km/hr
+                  </li>
+                  <li>
+                    <img
+                      src="http://openweathermap.org/img/wn/02d@2x.png"
+                      alt="humidity-icon"
+                      class="detail-icons"
+                    />
+                    {weatherData.humidity} %
+                  </li>
+                  <li>
+                    <img
+                      src="http://openweathermap.org/img/wn/10d@2x.png"
+                      alt="real"
+                      class="detail-icons"
+                    />
+                    Feels like {weatherData.real}
+                    °C
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <Conversion />
+          </div>
+        </section>
+        <Footer />
       </div>
     );
   } else {
@@ -35,6 +86,6 @@ export default function Weather() {
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleSubmit);
 
-    return "Waiting for data...";
+    return "Searching for current weather...";
   }
 }
